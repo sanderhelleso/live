29823f563cd2f490c51e63b36a8d4211c49fec48
Sanderhelleso <sanderhelleso@hotmail.com>
2018-11-16 17:10:31 -0800

    added base folder structure and core files

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<?php include("head/head.php"); ?>
+<html>
+<body>
+<?php include("nav/nav.php"); ?>
+<?php include("landing/landing.php"); ?>
+<?php include("footer/footer.php"); ?>
+</body>
+</html>
---
+++ signup/signup.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<?php include("../head/signupHead.php"); ?>
+<html>
+<body>
+    <?php include("../nav/nav.php"); ?>
+    <h2>Signup</h2>
+    <?php include("signupForm.php"); ?>
+    <?php include("../footer/footer.php"); ?>
+</body>
+</html>
