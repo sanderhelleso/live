457138e54bd4ddb16791cd67cf48226af196e688
Sanderhelleso <sanderhelleso@hotmail.com>
2018-11-17 20:02:32 -0800

    added loginpage, form and styling aswell as media queries for responsiveness

---
+++ login/loginForm.php
@@ -0,0 +1,31 @@
+<form id="login-form-cont">
+    <div class="field">
+        <div class="control has-icons-left has-icons-right">
+            <input class="input is-medium" type="email" placeholder="E-Mail">
+            <span class="icon is-small is-left">
+            <i data-feather="mail"></i>
+            </span>
+        </div>
+    </div>
+
+    <div class="field">
+        <div class="control has-icons-left has-icons-right">
+            <input class="input is-medium" type="email" placeholder="Password">
+            <span class="icon is-left">
+            <i data-feather="lock"></i>
+            </span>
+        </div>
+    </div>
+    <div class="control has-text-centered is-medium">
+        <button id="login-btn" class="button is-primary">Log In</button>
+    </div>
+    <div class="checkbox-cont">
+        <label class="checkbox">
+        <input type="checkbox">
+        Remember me
+        </label>
+    </div>
+    <a href="#">Forgott password?</a>
+    <span class="login-options">or</span>
+    <a href="/signup/signup.php">Dont have an account?</a>
+</form>
