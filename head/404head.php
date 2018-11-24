c8b67f0dd7340e75766d263d4095ff3ab1ac70ab
Sanderhelleso <sanderhelleso@hotmail.com>
2018-11-24 02:25:56 -0800

    added 404 page

---
+++ head/404head.php
@@ -0,0 +1,48 @@
+<!-- Forgot Password Head component -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+    <!-- SEO META -->
+    <meta name="robots" content="follow, index" />
+    <meta name="publisher" content="https://github.com/sanderhelleso" />
+    <meta name="description" content="APP DESC">
+    <meta property="og:site_name" content="APP NAME">
+    <meta name="keyword" content="APP KEYWORDS">
+    <meta name="robots" content="index, follow">
+        
+    <!-- Google + -->
+    <meta itemprop="[Organization]">
+    <meta itemprop="name" content="APP NAME">
+    <meta itemprop="description" content="APP DESC">
+    <meta property="og:site_name" content="APP NAME">
+    <meta itemprop="image" content="/URL/APPCOVER">
+        
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="APP TITLE">
+    <meta name="twitter:description" content="APP DESC">
+    <meta property="og:site_name" content="APP TITLE">
+    <meta name="twitter:image" content="/URL/APPCOVER">
+    <meta name="twitter:url" content="APP URL">
+        
+    <!-- Facebook -->
+    <meta property="og:title" content="APP NAME">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="APP URL">
+    <meta property="og:image" content="/URL/APPCOVER">
+    <meta property="og:description" content="APP DESC">
+    <meta property="og:site_name" content="APP NAME"> 
+        
+    <link rel="icon" href="/URL/TO/ICON" type="image/gif" sizes="16x16">
+
+    <link rel="stylesheet" type="text/css" media="screen" href="../public/css/lib/animate.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../public/css/lib/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../public/css/main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../public/css/404.css" />
+    
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+    <script src="../public/js/navbar.js"></script>
+
+    <title>404 - Page Not Found | LIVE</title>
+</head>
