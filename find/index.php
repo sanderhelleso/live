c92b33aa6530adc355eed87b7f57c42208c39f36
Zackery Hutchinson <zhutchinson@csumb.edu>
2018-11-29 22:10:28 +0000

    find and map

---
+++ find/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <?php include("../head/findhead.php") ?>
+    </head>
+    <div class="map"><?php include("findmap.php") ?> </div>
+    <div class="form"></div>
+    <script async defersrc="https://maps.googleapis.com/maps/api/js?key=AIzaSyDKlOpnC8ZKf2LN1Q6MoP3ZS4TmGTGYeho&callback=initMap"></script>
+</html>
