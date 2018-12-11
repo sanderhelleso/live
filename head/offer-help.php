1f1af0d73e70f1c04724c7b11a6dd0647b45800a
Gabriel Sanchez <gsanchez@csumb.edu>
2018-12-11 09:30:58 +0000

    Fixing overview.php

--- head/offer-help.php
+++ head/offer-help.php
@@ -12,27 +12,27 @@
     <meta itemprop="name" content="LIVE">
     <meta itemprop="description" content="Live your life. Find qualified and passionate people to tke care of those you care about.">
     <meta property="og:site_name" content="LIVE">
-    <meta itemprop="image" content="../public/img/landing/hero.jpg">
+    <meta itemprop="image" content="../../public/img/landing/hero.jpg">
         
     <!-- Twitter -->
     <meta name="twitter:card" content="summary">
     <meta name="twitter:title" content="LIVE">
     <meta name="twitter:description" content="Live your life. Find qualified and passionate people to tke care of those you care about.">
     <meta property="og:site_name" content="LIVE">
-    <meta name="twitter:image" content="../public/img/landing/hero.jpg">
+    <meta name="twitter:image" content="../../public/img/landing/hero.jpg">
     <meta name="twitter:url" content="https://demoliveapp.herokuapp.com/">
         
     <!-- Facebook -->
     <meta property="og:title" content="LIVE">
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://demoliveapp.herokuapp.com/">
-    <meta property="og:image" content="../public/img/landing/hero.jpg">
+    <meta property="og:image" content="../../public/img/landing/hero.jpg">
     <meta property="og:description" content="Live your life. Find qualified and passionate people to tke care of those you care about.">
     <meta property="og:site_name" content="LIVE"> 
         
-    <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="../public/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../public/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../../public/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../public/favicon/favicon-16x16.png">
     
     <link rel="stylesheet" type="text/css" media="screen" href="../../public/css/lib/animate.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="../../public/css/lib/bulma.min.css" />
