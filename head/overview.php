5bb1d0f409a75962002b0b35c78220b8597e2419
Gabriel Sanchez <gsanchez@csumb.edu>
2018-12-11 09:55:32 +0000

    Fixing the head for all these files

--- head/overview.php
+++ head/overview.php
@@ -1,3 +1,7 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <!-- SEO META -->
     <meta name="robots" content="follow, index" />
     <meta name="publisher" content="https://github.com/sanderhelleso" />
